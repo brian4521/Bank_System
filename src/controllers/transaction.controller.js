4397b4d3c917ee5876ffa68fb03fd0cb9a8d27d0
donald <warnerroby6@gmail.com>
2026-07-08 21:55:14 +0545

    working with transaction controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -4,6 +4,10 @@ const accountModel = require('../models/account.model');
 
 
 async function createTransaction(req,res){
+
+  /**
+   * - validate request
+   */
   
   const {fromAccount, toAccount, amount, idempotencyKey} = req.body
 
@@ -27,8 +31,34 @@ async function createTransaction(req,res){
     })
   }
 
+  /**
+   * - check if transaction with the same idempotencyKey already exists
+   */
+  const isTransactionAlreadyExists = await transactionModel.findOne({
+    idempotencyKey:idempotencyKey
+  })
+
+  if(isTransactionAlreadyExists){
+    if(isTransactionAlreadyExists.status === "completed"){
+      return res.status(400).json({
+        message:"Transaction already processed with the same idempotencyKey",
+        transaction:isTransactionAlreadyExists
+      })
+    }
+     
+    if(isTransactionAlreadyExists.status === "pending"){
+      return res.status(400).json({
+        message:"Transaction is already in progress with the same idempotencyKey",
+        
+      })
+    }
+    if(isTransactionAlreadyExists.status === "failed"){
+      return res.status(400).json({
+        message:"Transaction failed with the same idempotencyKey",
+        transaction:isTransactionAlreadyExists
+      })
+    }
+  }
 
 }
-
-
 //return res.status(201).json({ should be included so that no other response below this line is executed and the response is sent to the client
